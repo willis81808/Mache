96fb92779bf32d2ec28f71d9157f7e7f89b7b98b
willis81808 <willis81808@gmail.com>
2023-03-07 14:43:35 -0700

    `EventDispatcher` now works in offline games

--- Mache/Networking/EventDispatcher.cs
+++ Mache/Networking/EventDispatcher.cs
@@ -42,23 +42,54 @@ namespace Mache.Networking
             eventData.Add(registration);
         }
 
-        public static void RaiseEvent<T>(T payload) where T : SimpleEvent<T>
+        public static void RaiseEvent<T>(T payload, Bolt.GlobalTargets targets = Bolt.GlobalTargets.Everyone) where T : SimpleEvent<T>
         {
-            RaiseEvent(SimpleEvent<T>.GetEventID(), payload);
+            RaiseEvent(SimpleEvent<T>.GetEventID(), payload, targets);
         }
 
-        public static void RaiseEvent(string eventId, object payload)
+        public static void RaiseEvent(string eventId, object payload, Bolt.GlobalTargets targets = Bolt.GlobalTargets.Everyone)
         {
             if (eventData.FirstOrDefault(d => d.EventId == eventId) is EventRegistration data)
             {
                 var serialized = data.SerializeData?.Invoke(payload);
 
-                var eventToRaise = debugCommand.Raise(Bolt.GlobalTargets.Everyone);
+                if (!BoltNetwork.isConnected && TargetIncludesSelf(targets))
+                {
+                    OnReceiveEvent(eventId, serialized);
+                    return;
+                }
+
+                var eventToRaise = debugCommand.Raise(targets);
                 eventToRaise.input = eventId;
                 eventToRaise.input2 = serialized;
 
                 eventToRaise.Send();
             }
+        }
+
+        private static bool TargetIncludesSelf(Bolt.GlobalTargets targets)
+        {
+            switch (targets)
+            {
+                case Bolt.GlobalTargets.AllClients:
+                    return true;
+                case Bolt.GlobalTargets.Everyone:
+                    return true;
+                case Bolt.GlobalTargets.OnlySelf:
+                    return true;
+                case Bolt.GlobalTargets.Others:
+                    return false;
+                case Bolt.GlobalTargets.OnlyServer:
+                    if (BoltNetwork.isConnected)
+                    {
+                        return BoltNetwork.isServer;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+            }
+            return false;
         }
 
         internal static void OnReceiveEvent(string id, string payload)
